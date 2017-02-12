532c01d1526ef430c8154a7e6801299554e12130
swcgogo0308 <swcgogo0308@naver.com>
2017-02-13 03:50:05 +0900

    Ability HelpDialog

---
+++ Assets/Scripts/UI/UpgradeHelp.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeHelp : MonoBehaviour {
+
+    void Start()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+
+	public void ClickQuestionMark()
+    {
+        this.gameObject.SetActive(true);
+    }
+
+    public void ClickButton()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
