8ab10903b4f02d69d8a63903f6b53e597c5935ca
swcgogo0308 <swcgogo0308@naver.com>
2017-02-12 21:59:14 +0900

    FadeOutBgm

---
+++ Assets/Scripts/Util/FadeOutVolume.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeOutVolume : MonoBehaviour {
+
+    public AudioSource audio;
+
+	public void Click()
+    {
+        StartCoroutine(StartGame());
+    }
+
+    private IEnumerator StartGame()
+    {
+        float j = 1f;
+        for (float i = 0; i < 100; i ++)
+        {
+            if (j > 0)
+                j -= 0.09f;
+            audio.volume = j;
+            yield return null;
+        }
+    }
+}
